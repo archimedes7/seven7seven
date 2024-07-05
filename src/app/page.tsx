e4170361cba1888be796388d8cf594c11f0d1b04
archimedes7 <ggalang@seven7trades.com>
2024-07-06 00:08:16 +0800

    @SEV5-Initial frontend from Design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,73 @@
 import Image from "next/image";
+import Link from "next/link";
+import { GlobeDemo } from "./components/GlobeDemo";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="min-h-screen bg-blue-50">
+      <header className="container mx-auto px-4 py-6 flex items-center justify-between">
+        <div className="flex items-center">
+          <Image
+            src="/sevenlogo.png"
+            alt="Seven Trades Logo"
+            width={40}
+            height={40}
+          />
+          <span className="ml-2 font-bold">SEVEN7TRADES</span>
         </div>
-      </div>
+        <button className="bg-blue-100 text-black px-4 py-2 rounded-md hover:bg-gray-100">
+          Join us
+        </button>
+      </header>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
+      <main className="container mx-auto px-4 py-12 md:flex items-center">
+        <div className="md:w-1/2">
+          <h1 className="text-4xl md:text-5xl font-bold mb-4">
+            Tailor your digital space
+          </h1>
+          <p className="mb-6">
+            Leveraging open-source technology to provide cybersecurity tools,
+            digital forensic analysis free - legal scholar support for law
+            students, IN-HOUSE Legal fo Business and innovative transport
+            solutions through open-source applications and specialised services.
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+        </div>
+        <div className="md:w-1/2 mt-8 md:mt-0">
+          <Image
+            src="/tailor.png"
+            alt="Digital Space Illustration"
+            width={500}
+            height={400}
+          />
+        </div>
+      </main>
+      <section>
+        <GlobeDemo />
+      </section>
+      <footer className="bg-blue-50 text-black py-6 mt-8">
+        <div className="container mx-auto px-4 text-center">
+          <p className="mb-4">
+            We extend our heartfelt thanks to our sponsors and acknowledge the
+            incredible contributions of the following open-source projects:
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+          <div className="flex flex-wrap justify-center items-center space-x-4 mb-4">
+            <span className="text-xs md:text-sm lg:text-base">Python</span>
+            <span className="text-xs md:text-sm lg:text-base">Next.js</span>
+            <span className="text-xs md:text-sm lg:text-base">Wazuh</span>
+            <span className="text-xs md:text-sm lg:text-base">OLLama</span>
+            <span className="text-xs md:text-sm lg:text-base">OpenWebUI</span>
+            <span className="text-xs md:text-sm lg:text-base">
+              Aceternity UI
             </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            <span className="text-xs md:text-sm lg:text-base">OAuth</span>
+          </div>
+          <div className="flex flex-wrap justify-center items-center space-x-4">
+            <Image src="" alt="Logo 1" width={40} height={40} className="m-2" />
+            <Image src="" alt="Logo 2" width={40} height={40} className="m-2" />
+            {/* Add more logos as needed */}
+          </div>
+        </div>
+      </footer>
+    </div>
   );
 }
